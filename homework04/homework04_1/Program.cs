da013db82ee22763e0e048a8ac6133f321be8348
赵靖扬 <1426407876@qq.com>
2021-03-24 00:18:57 +0800

    homework04

---
+++ homework04/homework04_1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework04_1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> n = this.head;
+            while (n != null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> inilist = new GenericList<int>();
+            for(int i=0;i<10;i++)
+            {
+                inilist.Add(i);
+            }
+            Console.WriteLine("打印链表元素:");
+            inilist.ForEach(m => Console.Write(m+" "));
+            Console.WriteLine();
+            int max = 0;
+            inilist.ForEach(m => { if (m > max) max = m; });
+            Console.WriteLine($"最大值:{max}");
+            int min = 0;
+            inilist.ForEach(m => { if (m < min) min = m; });
+            Console.WriteLine($"最小值:{min}");
+            int sum = 0;
+            inilist.ForEach(m => sum += m);
+            Console.WriteLine($"和:{sum}");
+
+        }
+    }
+}
---
+++ homework04/homework04_2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework04_2
+{
+    public delegate void TimeHandler(object sender, TimeEventArgs args);
+    public class TimeEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+    public class Clock
+    {
+        public event TimeHandler OnTick;
+        public event TimeHandler OnAlarm;
+        public int hour, minute, second;
+        public int alarmHour, alarmMinute;
+        public Clock(int hour,int minute,int second) 
+        {
+            if(hour<24&&hour>=0&&minute<60&&minute>=0&&second<60&&second>0)
+            {
+                this.hour = hour;
+                this.minute = minute;
+                this.second = second;
+            }
+            else
+            {
+                this.hour = 0;
+                this.minute = 0;
+                this.second = 0;
+            }
+        }
+        public void SetTime(int alarmHour,int alarmMinute) //设置闹钟
+        {
+            this.alarmHour = alarmHour;
+            this.alarmMinute = alarmMinute;
+        }
+        public void Running()
+        {
+            while(true)
+            {
+                this.second++;
+                if(this.second==60)
+                {
+                    this.second = 0;
+                    this.minute++;
+                    if (this.minute == 60)
+                    {
+                        this.minute = 0;
+                        this.hour++;
+                        if (this.hour == 24)
+                            this.hour = 0;
+                    }
+                }
+                TimeEventArgs args = new TimeEventArgs() { Hour = hour, Minute = minute, Second = second };
+                if (this.hour == this.alarmHour && this.minute == this.alarmMinute)
+                    OnAlarm(this,args);
+                else
+                    OnTick(this,args);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+    public class AlarmClock
+    {
+        public Clock clock;
+        public AlarmClock(int hour,int minute,int second) //订阅事件
+        {
+            this.clock = new Clock(hour, minute, second);
+            this.clock.OnTick += OnTick;
+            this.clock.OnAlarm += OnAlarm;
+        }
+        public void OnTick(object sender,TimeEventArgs args) 
+        {
+            Console.WriteLine($"{args.Hour}:{args.Minute}:{args.Second}");
+        }
+        public void OnAlarm(object sender, TimeEventArgs args)
+        {
+            Console.WriteLine($"Alarm!!!!!!!Now is{args.Hour}:{args.Minute}:{args.Second}");
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock alarmClock = new AlarmClock(16,30,30);
+            alarmClock.clock.SetTime(16, 31);
+            alarmClock.clock.Running();
+        }
+    }
+}
