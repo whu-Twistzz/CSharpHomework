53af065a508617ba66b4c25d12ad68005d1b5adb
赵靖扬 <1426407876@qq.com>
2021-04-15 18:17:34 +0800

    homework01

---
+++ homework07/homework07_1/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace homework07_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.create_btn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.color_btn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.th2Text = new System.Windows.Forms.TextBox();
+            this.th1Text = new System.Windows.Forms.TextBox();
+            this.per2Text = new System.Windows.Forms.TextBox();
+            this.per1Text = new System.Windows.Forms.TextBox();
+            this.lengText = new System.Windows.Forms.TextBox();
+            this.NText = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 498);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.create_btn);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.splitContainer1.Size = new System.Drawing.Size(867, 498);
+            this.splitContainer1.SplitterDistance = 277;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // create_btn
+            // 
+            this.create_btn.Location = new System.Drawing.Point(91, 452);
+            this.create_btn.Name = "create_btn";
+            this.create_btn.Size = new System.Drawing.Size(90, 34);
+            this.create_btn.TabIndex = 3;
+            this.create_btn.Text = "生成";
+            this.create_btn.UseVisualStyleBackColor = true;
+            this.create_btn.Click += new System.EventHandler(this.create_btn_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel2);
+            this.groupBox2.Controls.Add(this.color_btn);
+            this.groupBox2.Location = new System.Drawing.Point(3, 286);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(271, 148);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "画笔参数";
+            // 
+            // color_btn
+            // 
+            this.color_btn.Location = new System.Drawing.Point(95, 119);
+            this.color_btn.Name = "color_btn";
+            this.color_btn.Size = new System.Drawing.Size(75, 23);
+            this.color_btn.TabIndex = 0;
+            this.color_btn.Text = "选择颜色";
+            this.color_btn.UseVisualStyleBackColor = true;
+            this.color_btn.Click += new System.EventHandler(this.color_btn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.th2Text);
+            this.groupBox1.Controls.Add(this.th1Text);
+            this.groupBox1.Controls.Add(this.per2Text);
+            this.groupBox1.Controls.Add(this.per1Text);
+            this.groupBox1.Controls.Add(this.lengText);
+            this.groupBox1.Controls.Add(this.NText);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 277);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cayley树参数";
+            // 
+            // th2Text
+            // 
+            this.th2Text.Location = new System.Drawing.Point(102, 230);
+            this.th2Text.Name = "th2Text";
+            this.th2Text.Size = new System.Drawing.Size(100, 21);
+            this.th2Text.TabIndex = 11;
+            // 
+            // th1Text
+            // 
+            this.th1Text.Location = new System.Drawing.Point(102, 192);
+            this.th1Text.Name = "th1Text";
+            this.th1Text.Size = new System.Drawing.Size(100, 21);
+            this.th1Text.TabIndex = 10;
+            // 
+            // per2Text
+            // 
+            this.per2Text.Location = new System.Drawing.Point(102, 154);
+            this.per2Text.Name = "per2Text";
+            this.per2Text.Size = new System.Drawing.Size(100, 21);
+            this.per2Text.TabIndex = 9;
+            // 
+            // per1Text
+            // 
+            this.per1Text.Location = new System.Drawing.Point(102, 116);
+            this.per1Text.Name = "per1Text";
+            this.per1Text.Size = new System.Drawing.Size(100, 21);
+            this.per1Text.TabIndex = 8;
+            // 
+            // lengText
+            // 
+            this.lengText.Location = new System.Drawing.Point(102, 78);
+            this.lengText.Name = "lengText";
+            this.lengText.Size = new System.Drawing.Size(100, 21);
+            this.lengText.TabIndex = 7;
+            // 
+            // NText
+            // 
+            this.NText.Location = new System.Drawing.Point(102, 40);
+            this.NText.Name = "NText";
+            this.NText.Size = new System.Drawing.Size(100, 21);
+            this.NText.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "左分支角度";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "右分支角度";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "左分支长度比";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "右分支长度比";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "主干长度";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "递归深度";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(580, 495);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Blue;
+            this.panel2.Location = new System.Drawing.Point(36, 20);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 93);
+            this.panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 498);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox th2Text;
+        private System.Windows.Forms.TextBox th1Text;
+        private System.Windows.Forms.TextBox per2Text;
+        private System.Windows.Forms.TextBox per1Text;
+        private System.Windows.Forms.TextBox lengText;
+        private System.Windows.Forms.TextBox NText;
+        private System.Windows.Forms.Button create_btn;
+        private System.Windows.Forms.Button color_btn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ homework07/homework07_1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework07_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private Graphics graphics;
+        static double th1 = 30 * Math.PI / 180;
+        static double th2 = 20 * Math.PI / 180;
+        static double per1 = 0.6;
+        static double per2 = 0.7;
+
+        static int n=0;
+        static double leng=0;
+        Color col;
+        
+        void drawCayLeyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+
+            drawCayLeyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayLeyTree(n - 1, x1, y1, per2 * leng, th - th2);
+
+        }
+        private void create_btn_Click(object sender, EventArgs e)
+            
+        {
+            getMessage();
+            if (graphics == null) graphics = panel1.CreateGraphics();
+            graphics.Clear(BackColor);
+            drawCayLeyTree(n,270,380,leng, -Math.PI / 2);
+        }
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(new Pen(Color.FromArgb(col.R,col.G,col.B)), (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        void getMessage()
+        {
+            n = Convert.ToInt32(NText.Text);
+            leng = Convert.ToDouble(lengText.Text);
+            per1 = Convert.ToDouble(per1Text.Text);
+            per2 = Convert.ToDouble(per2Text.Text);
+            th1 = Convert.ToDouble(th1Text.Text) * Math.PI / 180;
+            th2 = Convert.ToDouble(th2Text.Text) * Math.PI / 180;
+        }
+
+        private void color_btn_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = colorDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                col = colorDialog1.Color;
+                panel2.BackColor = col;
+            }
+        }
+    }
+}
